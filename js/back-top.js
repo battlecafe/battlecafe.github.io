e60781a7e8c6f730fadb4bd71c82f6545242ab7f
FAN Yu <battlecafe@163.com>
2017-02-09 05:43:34 +0300

    I

--- js/back-top.js
+++ js/back-top.js
@@ -1,15 +1,37 @@
-BackTop=function(btnId){
-	var btn=document.getElementById(btnId);
-	var d=document.documentElement;
-	window.onscroll=set;
-	btn.onclick=function (){
-		btn.style.display="none";
-		window.onscroll=null;
-		this.timer=setInterval(function(){
-			d.scrollTop-=Math.ceil(d.scrollTop*0.1);
-			if(d.scrollTop==0) clearInterval(btn.timer,window.onscroll=set);
-		},10);
-	};
-	function set(){btn.style.display=d.scrollTop?'block':"none"}
-};
-BackTop('back-top');
+﻿//页面加载完毕后触发
+window.onload = function(){
+	var obtn = document.getElementById("back-top");
+	//获取页面可视区的高度
+	var clientHeight = document.documentElement.clientHeight;
+	var timer = null; 
+	var isTop = true; //回到顶部的过程中如果滚动鼠标滚条会暂停
+	
+	//滚动条滚动时触发
+	window.onscroll = function(){
+		//获取滚动条距离顶部的高度，IE:document.documentElement.scrollTop  chrome:document.body.scrollTop
+		var osTop = document.documentElement.scrollTop || document.body.scrollTop;
+		if(osTop >= clientHeight){
+			obtn.style.display = 'block';
+		}else{
+			obtn.style.display = 'none';
+		}
+		if(!isTop){
+			clearInterval(timer);
+		}
+		isTop = false;
+	}
+	
+	obtn.onclick = function(){
+		
+		//设置定时器
+		timer = setInterval(function(){
+			var osTop = document.documentElement.scrollTop || document.body.scrollTop;
+			var speed = Math.floor(-osTop / 6);  //速度随距离动态变化，越来越小
+			document.documentElement.scrollTop = document.body.scrollTop = osTop + speed;
+			isTop = true;
+			if(osTop == 0){
+				clearInterval(timer); //回到顶部时关闭定时器
+			}
+		},30)
+	}
+}
