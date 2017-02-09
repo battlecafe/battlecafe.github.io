0a259b7217cbdfdf4cda9caaa7e60e7a0e253a28
FAN Yu <battlecafe@163.com>
2017-02-09 06:16:55 +0300

    I

--- js/back-top.js
+++ js/back-top.js
@@ -2,13 +2,13 @@ $("#back-top").hide();
 $(document).ready(function () {
   $(window).scroll(function () {
     if ($(this).scrollTop() > 100) {
-      $('#back-top').fadeIn(800);
+      $('#back-top').fadeIn();
     } else {
-      $('#back-top').fadeOut(800);
+      $('#back-top').fadeOut();
     }
   });
   $('#back-top a').click(function () {
-    $('html,body').animate({
+    $('html,body,nav').animate({
       scrollTop: 0
     }, 800);
     return false;
